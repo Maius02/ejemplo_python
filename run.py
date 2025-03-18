4f0dd3e5a93d673254612fe2e0cff3de943a9b2a
maius02 <maiapaez02@gmail.com>
2025-03-17 21:18:48 -0300

    modifico app y creacion de README para documentacion

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+#seminario de python
+#ejercicio 2 practica 1
 grados_c = int( input("Ingrese temperatura a convertir (°C) " ) ) 
 grados_f = ( grados_c * 9 ) / 5 + 32
 print ( str(grados_c) + "°C equivalente a " + str(grados_f) + "°F")
